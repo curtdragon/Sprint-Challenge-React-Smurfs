981cf05f137f371af958b2d5ba11ee84969d0d61
curtdragon <tealturboser@gmail.com>
2019-06-30 21:17:03 -0500

    Complete listing of Smurfs. Abilty to add smurfs working

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import Smurf from "./components/Smurf";
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -11,15 +13,40 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({
+          smurfs: response.data
+        });
+      })
+      .catch(err => {
+        console.log('Error:', err)
+      });
+  }
+
+  updateSmurfs = (smurfs) => {
+    this.setState({ smurfs })
+  }
+  
   render() {
+    const { smurfs } = this.state;
+
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <div>
+            <Link to="/smurfs">Smurfs</Link>
+            <Link to="/smurf-form">SmurfForm</Link>
+          </div>
+        </nav>
+      
+        <Route path="/smurfs" exact render={(props) => <Smurfs {...props} smurfs={smurfs} />} />
+        <Route path="/smurfs/:id" render={(props) => <Smurf {...props} smurfs={smurfs} />} />
+        <Route path="/smurf-form" exact render={(props) => <SmurfForm {...props} updateSmurfs={this.updateSmurfs} />} />
       </div>
+
     );
   }
 }
